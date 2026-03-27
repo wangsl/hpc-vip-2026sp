83e96e484bf1de624d18f2261cd4dcda014bbc65
wangsl <wangsl2001@gmail.com>
2026-03-27 12:13:23 -0400

    add netID accounts

--- aws-startup.bash
+++ aws-startup.bash
@@ -1,7 +1,39 @@
 #!/bin/bash
+
+
+function create_user_accounts()
+{
+  local flag=
+  local line=
+  while read -r line; do
+    local IFS=":"
+    local line=($line)
+    local netID=${line[0]}
+    local uID=${line[2]}
+    local gID=${line[3]}
+    local name=${line[4]}
+
+    if [[ -d /home/${netID} ]]; then flag="-M"; fi
+
+    /usr/sbin/groupadd -g ${gID} ${netID}
+    /usr/sbin/useradd ${flag} -g ${gID} -s /bin/bash -u ${uID} -c "${name}" ${netID}
+  done <<EOF
+kc4207:x:3334730:3334730:Kevin Chan:/home/kc4207:/bin/bash
+np3129:x:4441975:4441975:Nishant Pushparaju:/home/np3129:/bin/bash
+np3129:x:4441975:4441975:Nishant Pushparaju:/home/np3129:/bin/bash
+tc1841:x:2420783:2420783:Taidi Chen:/home/tc1841:/bin/bash
+tc4119:x:4284622:4284622:Robin Chen:/home/tc4119:/bin/bash
+tc4119:x:4284622:4284622:Robin Chen:/home/tc4119:/bin/bash
+sw77:x:1296493:1296493:Shenglong Wang:/home/sw77:/bin/bash
+EOF
+
+  unset IFS
+}
+
 (
-echo "Hello world" 
-hostname
-env | sort -u
-yum install emacs -y
+  echo "Hello world" 
+  hostname
+  env | sort -u
+  create_user_accounts
 ) >> /tmp/nyu-startup.log 2>&1
+
